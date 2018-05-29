63161b0b8d4b5384d705f3570b7fb580e7469bdf
T-101 <T-101@PC229>
2018-05-28 19:46:06 -0500

    Constructores

---
+++ objetos-1803/src/constructores/Fruta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class Fruta {
+
+    String nombre;
+    float precio;
+
+    public Fruta() {
+        nombre = "Persimo";
+
+    }
+
+}
---
+++ objetos-1803/src/constructores/ProbarConstructores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarConstructores {
+
+    public static void main(String[] args) {
+        // Vamos a crear una fruta
+        Fruta f1 = new Fruta();
+        System.out.println("El nombre de la fruta es: " + f1.nombre);
+        System.out.println("El precio de la fruta es: " + f1.precio);
+
+        // float valor;
+        // System.out.println(valor);
+    }
+}
