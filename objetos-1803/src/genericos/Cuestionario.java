812e6f90a1edc4ac1a4516e3e11a24ee0d38a796
T-101 <T-101@PC229>
2018-06-07 19:42:51 -0500

    Arreglos Genéricos

---
+++ objetos-1803/src/genericos/Cuestionario.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genericos;
+
+/**
+ *
+ * @author T-101
+ */
+import java.util.*;
+
+public class Cuestionario {
+    private ArrayList<Pregunta> preguntas;
+    private float resultado;
+
+    public ArrayList<Pregunta> getPreguntas() {
+        return preguntas;
+    }
+
+    public void setPreguntas(ArrayList<Pregunta> preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public float getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(float resultado) {
+        this.resultado = resultado;
+    }
+}
